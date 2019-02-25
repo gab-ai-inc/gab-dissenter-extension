a28fafdeb2c52fd0bf696326df3054c05e5a733e
mgabdev <dev.mi.ma.rk22@gmail.com>
2019-02-25 02:35:01 -0500

    Added util scripts
    • Added:
    - constants.js
    - helpers.js

---
+++ src/scripts/utils/constants.js
@@ -0,0 +1,8 @@
+/* BROWSERS */
+var BROWSER_CHROME_SLUG = 'chrome';
+var BROWSER_FIREFOX_SLUG = 'firefox';
+var BROWSER_EDGE_SLUG = 'edge';
+var BROWSER_SAFARI_SLUG = 'safari';
+
+/* BASE URI FOR DISSENTER EXTENSION */
+var BASE_URI = 'https://dissenter.com/discussion/begin-extension?url=';
---
+++ src/scripts/utils/helpers.js
@@ -0,0 +1,41 @@
+/**
+ * @description Internal helper to check if parameter is an object
+ * @function isObject
+ * @param {*} obj
+ * @returns {boolean}
+ */
+var isObject = function(obj) {
+    if (isArray(obj) || isDate(obj)) return false;
+    return (obj !== null && typeof obj === 'object');
+};
+
+/**
+ * @description Internal helper to check if parameter is a string
+ * @function isString
+ * @param {*} str
+ * @returns {boolean}
+ */
+var isString = function(str) {
+    return (typeof str === 'string' || str instanceof String);
+};
+
+/**
+ * @description Internal helper to check if parameter is a date
+ * @function isDate
+ * @param {*} date
+ * @returns {boolean}
+ */
+var isDate = function(date) {
+    if (isString(date) || isArray(date) || date == undefined || date == null) return false;
+    return (date && Object.prototype.toString.call(date) === "[object Date]" && !isNaN(date));
+};
+
+/**
+ * @description Internal helper to check if parameter is an array
+ * @function isArray
+ * @param {*} arr
+ * @returns {boolean}
+ */
+var isArray = function(arr) {
+    return Array.isArray(arr);
+};
