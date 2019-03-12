de74f7d177ddbe6bc32891afc285c0044d5c6b1f
mgabdev <47340266+mgabdev@users.noreply.github.com>
2019-03-11 23:50:20 -0400

    Added browser runtime message function
    • Added:
    - browser runtime message function to listen for messages.
    - BACKGROUND_ACTION_OPEN_POPUP message action to open new popup window

--- src/scripts/background/runtime.js
+++ src/scripts/background/runtime.js
@@ -4,3 +4,38 @@
 __BROWSER__.browserAction.onClicked.addListener(function() {
     __BROWSER__.tabs.create({ url: 'index.html' });
 });
+
+/**
+ * @description - onMessage Handler for sending messages from elsewhere to this background.js file
+ * @returns callback(*)
+ */
+__BROWSER__.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    var action = message['action'] || '';
+
+    if (action === BACKGROUND_ACTION_OPEN_POPUP) {
+        //Get url, height
+        var url = message['url'] || '';
+        var windowHeight = message['height'] || 0;
+
+        //Set variables
+        var screenWidth = screen.width;
+        var screenHeight = screen.height;
+        var top = screenHeight - windowHeight;
+
+        //Get popup url
+        var popupURL = __BROWSER__.extension.getURL("popup/popup.html?url=" + url);
+
+        //Open new popup window with url using popup.html
+        __BROWSER__.windows.create({
+            url: popupURL,
+            width: 420,
+            height: windowHeight,
+            top: top,
+            left: screenWidth,
+            type: 'popup'
+        });
+    }
+
+    //Async
+    return true;
+});
