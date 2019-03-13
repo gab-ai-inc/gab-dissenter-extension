986ed2b3fae407f26504d4256b767cacbe0cd378
mgabdev <47340266+mgabdev@users.noreply.github.com>
2019-03-13 01:32:57 -0400

    Added action, storage constants
    • Added:
    - action, storage constants

--- src/scripts/utils/constants.js
+++ src/scripts/utils/constants.js
@@ -9,6 +9,22 @@ var BASE_URI = 'https://dissenter.com/discussion/begin-extension?url=';
 
 /* ACTIONS */
 var BACKGROUND_ACTION_OPEN_POPUP = 'open_popup';
+var BACKGROUND_ACTION_GET_KEY = 'get_key';
+var BACKGROUND_ACTION_SET_KEY = 'set_key';
 
 /* COLORS */
 var COLOR_GAB_GREEN = '#21cf7b';
+
+/* STORAGE */
+var STORAGE_BASE = 'gab_dissenter_extension_data';
+
+var TWITTER_BUTTONS_ENABLED = 'twitter_buttons_enabled';
+var REDDIT_BUTTONS_ENABLED = 'reddit_buttons_enabled';
+var YOUTUBE_BUTTONS_ENABLED = 'youtube_buttons_enabled';
+
+var STORAGE_DEFAULT_PARAMS = {};
+STORAGE_DEFAULT_PARAMS[TWITTER_BUTTONS_ENABLED] = true;
+STORAGE_DEFAULT_PARAMS[REDDIT_BUTTONS_ENABLED] = true;
+STORAGE_DEFAULT_PARAMS[YOUTUBE_BUTTONS_ENABLED] = true;
+
+var STORAGE_KEY_ALL = 'all';
