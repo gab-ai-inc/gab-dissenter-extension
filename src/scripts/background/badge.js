b58b1e84af0c948bf7a14e5598fec49ff74d522c
mgabdev <47340266+mgabdev@users.noreply.github.com>
2019-03-24 00:45:36 -0400

    Added background badge extension functionality
    • Added:
    - background badge extension functionality

---
+++ src/scripts/background/badge.js
@@ -0,0 +1,71 @@
+/**
+ * @description - Set extension icon badge with text
+ * @param {String} text
+ */
+function setExtensionIconBadge(text) {
+    if (!text) text = '';
+    text = String(text);
+    text = formatBadgeText(text);
+
+    __BROWSER__.browserAction.setBadgeBackgroundColor({
+        color: "#f40e1c"
+    });
+
+    __BROWSER__.browserAction.setBadgeText({
+        text: text,
+        tabId: -1
+    });
+};
+
+/**
+ * @description - Helper to format badge text depending on different lengths
+ * @param {String} text
+ * @returns {String} formattedText
+ */
+function formatBadgeText(text) {
+    //Text must exist
+    if (!text) return '';
+
+    //Convert to number
+    var num = parseInt(text);
+    //If doesn't exist, return nothing
+    if (num < 1) return '';
+
+    //Placeholder
+    var formattedText = '';
+
+    //Check for lengths
+    if (num < 1000) {
+        return String(num);
+    }
+    else if (num >= 1000 && num < 10000) {
+        var rounded = truncateNumber(num, 2);
+        rounded = rounded / 10;
+        formattedText = rounded + 'k';
+    }
+    else if (num >= 10000 && num < 100000) {
+        var rounded = truncateNumber(num, 2);
+        formattedText = rounded + 'k';
+    }
+    else if (num >= 100000 && num < 1000000) {
+        var rounded = truncateNumber(num, 3);
+        formattedText = rounded + 'k';
+    }
+    else if (num >= 1000000) {
+        formattedText = '1M+';
+    }
+
+    //Return now
+    return formattedText;
+};
+
+/**
+ * @description - Helper to truncate number
+ * @param {Number} number
+ * @param {Number} truncate
+ * @param {String}
+ */
+function truncateNumber(number, truncate) {
+    if (!number) return 0;
+    return number.toString().substring(0, truncate);
+};
