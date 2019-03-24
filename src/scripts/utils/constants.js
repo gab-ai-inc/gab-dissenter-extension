27b5bfc9eac5445e32474a8618c8868640710ef1
mgabdev <47340266+mgabdev@users.noreply.github.com>
2019-03-24 00:46:11 -0400

    Added badge related constants
    • Added:
    - Badge related constants

--- src/scripts/utils/constants.js
+++ src/scripts/utils/constants.js
@@ -6,11 +6,14 @@ var BROWSER_SAFARI_SLUG = 'safari';
 
 /* BASE URI FOR DISSENTER EXTENSION */
 var BASE_URI = 'https://dissenter.com/discussion/begin-extension?url=';
+var COMMENT_COUNT_URI = "https://dissenter.com/notification/comment-count?url=";
 
 /* ACTIONS */
 var BACKGROUND_ACTION_OPEN_POPUP = 'open_popup';
 var BACKGROUND_ACTION_GET_KEY = 'get_key';
 var BACKGROUND_ACTION_SET_KEY = 'set_key';
+var BACKGROUND_ACTION_SET_BADGE = 'set_badge';
+var BACKGROUND_ACTION_TAB_UPDATED = 'tab_updated';
 
 /* COLORS */
 var COLOR_GAB_GREEN = '#21cf7b';
@@ -22,11 +25,13 @@ var TWITTER_BUTTONS_ENABLED = 'twitter_buttons_enabled';
 var REDDIT_BUTTONS_ENABLED = 'reddit_buttons_enabled';
 var YOUTUBE_BUTTONS_ENABLED = 'youtube_buttons_enabled';
 var WINDOW_SIDEBAR_UNAVAILABLE_ENABLED = 'window_sidebar_unavailable_enabled';
+var WEBSITE_COMMENT_BADGE_ENABLED = 'website_comment_badge_enabled';
 
 var STORAGE_DEFAULT_PARAMS = {};
 STORAGE_DEFAULT_PARAMS[TWITTER_BUTTONS_ENABLED] = true;
 STORAGE_DEFAULT_PARAMS[REDDIT_BUTTONS_ENABLED] = true;
 STORAGE_DEFAULT_PARAMS[YOUTUBE_BUTTONS_ENABLED] = true;
 STORAGE_DEFAULT_PARAMS[WINDOW_SIDEBAR_UNAVAILABLE_ENABLED] = true;
+STORAGE_DEFAULT_PARAMS[WEBSITE_COMMENT_BADGE_ENABLED] = true;
 
 var STORAGE_KEY_ALL = 'all';
