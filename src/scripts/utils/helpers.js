edc147d74fffdc1aa7f4ad8b8dad4a4e99902e70
mgabdev <47340266+mgabdev@users.noreply.github.com>
2019-04-24 12:24:19 -0400

    Added helper to get Dissenter logo as SVG
    For use throughout, content scripts, etc.

--- src/scripts/utils/helpers.js
+++ src/scripts/utils/helpers.js
@@ -58,4 +58,44 @@ function ready(fn) {
  */
 function getQueryStringValue(key) {
     return decodeURIComponent(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
-}
+};
+
+function getDissenterDLogoAsSVG(height, width, fillColor, backgroundColor) {
+    //Create "g" dissent icon
+    var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.style.setProperty("height", height, "important");
+    svg.style.setProperty("width", width, "important");
+    svg.setAttribute("version", "1.1");
+    svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    svg.setAttribute("xmlns:xlink", "http://www.w3.org/1999/xlink");
+    svg.setAttribute("viewBox", "0 0 500 500");
+
+    var g = document.createElementNS("http://www.w3.org/2000/svg", "g");
+    g.style.setProperty("transform", "translate(17, 37)", "important");
+
+    var solid = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    solid.setAttribute("d", "M244.885937,0 L193.657763,0 L65.5018905,0 L45.5665325,0 C20.4992438,0 0,20.5016933 0,45.5466667 L0,381.453333 C0,406.504 20.4992438,427 45.5665325,427 L65.5018905,427 L193.657763,427 L244.885937,427 C389.348933,427 467,326.3134 467,213.5 C467,100.6866 393.563837,0 244.885937,0");
+    solid.setAttribute("fill", fillColor);
+
+    var line1 = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    line1.setAttribute("d", "M309.275885,155 L121.729792,155 C115.246045,155 110,149.623395 110,143.002906 C110,136.376605 115.246045,131 121.729792,131 L309.275885,131 C315.759633,131 321,136.376605 321,143.002906 C321,149.623395 315.759633,155 309.275885,155");
+    line1.setAttribute("fill", backgroundColor);
+
+    var line2 = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    line2.setAttribute("d", "M309.275885,225 L121.729792,225 C115.246045,225 110,219.623395 110,213.002906 C110,206.376605 115.246045,201 121.729792,201 L309.275885,201 C315.759633,201 321,206.376605 321,213.002906 C321,219.623395 315.759633,225 309.275885,225");
+    line2.setAttribute("fill", backgroundColor);
+
+    var line3 = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    line3.setAttribute("d", "M121.78718,296 C115.271711,296 110,290.623395 110,284.002906 C110,277.376605 115.271711,272 121.78718,272 L247.218525,272 C253.733994,272 259,277.376605 259,284.002906 C259,290.623395 253.733994,296 247.218525,296 L121.78718,296 Z");
+    line3.setAttribute("fill", backgroundColor);
+
+    g.appendChild(solid);
+    g.appendChild(line1);
+    g.appendChild(line2);
+    g.appendChild(line3);
+
+    svg.appendChild(g);
+
+    return svg;
+};
+
